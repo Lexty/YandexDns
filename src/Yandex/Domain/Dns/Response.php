ef149c5917cfd3501fdd0f1bd0785174942622ea
Alexandr Medvedev <alexandr.mdr@gmail.com>
2014-10-09 08:05:50 +0400

    Added: some tests for Response class.

--- src/Yandex/Domain/Dns/Response.php
+++ src/Yandex/Domain/Dns/Response.php
@@ -11,6 +11,8 @@
  */
 namespace Yandex\Domain\Dns;
 
+use Yandex\Common\Exception\YandexException;
+
 /**
  * Class Response
  *
@@ -35,39 +37,64 @@ class Response
 
     /**
      * @param string|\SimpleXMLElement $response
+     * @return self $this
+     * @throws YandexException
      */
     public function __construct($response)
     {
         return $this->parseResponse($response);
     }
 
+    /**
+     * @return bool
+     */
     public function isSuccess()
     {
         return self::STATUS_SUCCESS === $this->status;
     }
 
+    /**
+     * @return string
+     */
     public function getStatus()
     {
         return $this->status;
     }
 
+    /**
+     * @return array
+     */
     public function getData()
     {
         return $this->data;
     }
 
+    /**
+     * @param string|\SimpleXMLElement $xml
+     * @return self $this
+     * @throws YandexException
+     * @codeCoverageIgnore
+     */
     protected function parseResponse($xml)
     {
         if (is_object($xml) && is_a($xml, '\SimpleXMLElement')) {
             $sxe = $xml;
         } else {
-            $sxe = $this->stringToSimpleXMLElement($xml);
+            $sxe = $this->stringToSimpleXML($xml);
         }
 
-        $this->status = (string) $sxe->domains->error;
+        /** @TODO Rewrite to php 5.5 */
+        // Check exists xml chain to 'error' node
+        if (1 === count($error = $sxe->xpath('/page/domains/error'))) {
+            $this->status = (string) $error[0];
+        } else {
+            throw new YandexException('Bad response');
+        }
 
-        if (null !== $sxe->domains->domain[0]->response->record) {
-            foreach ($sxe->domains->domain[0]->response->record as $record) {
+        /** @TODO Rewrite to php 5.5 */
+        // Check exists xml chain to 'record' nodes
+        if (0 !== count($records = $sxe->xpath('/page/domains/domain/response/record'))) {
+            foreach ($records as $record) {
                 $content = (string)$record;
                 $record = (array)$record;
                 $record = $record['@attributes'];
@@ -76,6 +103,9 @@ class Response
                 unset($record['id']);
                 $this->data[] = $record;
             }
+//        /** @TODO Rewrite to php 5.5 */
+//        } elseif (0 !== count($records = $sxe->xpath('/page/domains/domain/response'))) {
+//            throw new YandexException('Bad response');
         }
 
         return $this;
@@ -83,15 +113,16 @@ class Response
 
     /**
      * @param string $xml
-     * @throws \Exception
+     * @throws YandexException
      * @return \SimpleXMLElement
+     * @codeCoverageIgnore
      */
-    protected function stringToSimpleXMLElement($xml)
+    protected function stringToSimpleXML($xml)
     {
         libxml_use_internal_errors(true);
         $sxe = simplexml_load_string($xml);
         if (libxml_get_errors()) {
-            throw new \Exception('Bad response');
+            throw new YandexException('Bad response');
         }
         return $sxe;
     }
--- tests/Yandex/Tests/Domain/Dns/ResponseTest.php
+++ tests/Yandex/Tests/Domain/Dns/ResponseTest.php
@@ -8,14 +8,64 @@ use Yandex\Tests\Domain\Dns\Fixtures\Responses;
 
 class ResponseTest extends TestCase
 {
-    public function testResponse()
+    /**
+     * @param string $incomingData
+     * @param bool   $expectSuccess
+     * @param string $expectStatus
+     * @param array  $expectData
+     * @dataProvider correctResponseProvider
+     * @covers \Yandex\Domain\Dns\Response::__construct
+     * @covers \Yandex\Domain\Dns\Response::isSuccess
+     * @covers \Yandex\Domain\Dns\Response::getStatus
+     * @covers \Yandex\Domain\Dns\Response::getData
+     */
+    public function testCorrectResponse($incomingData, $expectSuccess, $expectStatus, $expectData)
     {
-        $fixtures = Responses::$responsesFixtures;
+        $response = new Response($incomingData);
+        $this->assertEquals($response->isSuccess(), $expectSuccess);
+        $this->assertEquals($response->getStatus(), $expectStatus);
+        $this->assertEquals($response->getData(),   $expectData);
+    }
 
-        $response = new Response($fixtures[0]);
-        $this->assertTrue($response->isSuccess());
+    public function correctResponseProvider()
+    {
+        $fixtures = Responses::responsesFixtures();
+        $expectData = array(array(
+            'domain' => 'yourdomain.ru',
+            'priority' => '',
+            'ttl' => 21600,
+            'subdomain' => 'www',
+            'type' => 'A',
+            'content' => '127.0.0.1',
+            'record_id' => 342432432,
+        ));
+        return array(
+            array($fixtures['correctSuccess'], true, 'ok', $expectData),
+            array($fixtures['correctError'], false, 'error', $expectData),
+            array($fixtures['correctEmptyData'], true, 'ok', array()),
+            array(simplexml_load_string($fixtures['correctSuccess']), true, 'ok', $expectData),
+            array(simplexml_load_string($fixtures['correctError']), false, 'error', $expectData),
+        );
+    }
 
-        $response = new Response($fixtures[1]);
-        $this->assertFalse($response->isSuccess());
+    /**
+     * @param string $incomingData
+     * @dataProvider incorrectResponseProvider
+     * @expectedException \Yandex\Common\Exception\YandexException
+     * @expectedExceptionMessage Bad response
+     * @covers \Yandex\Domain\Dns\Response::__construct
+     */
+    public function testIncorrectResponse($incomingData)
+    {
+        new Response($incomingData);
+    }
+
+    public function incorrectResponseProvider()
+    {
+        $fixtures = Responses::responsesFixtures();
+        return array(
+            array($fixtures['anotherStructure']),
+            array($fixtures['invalidXml']),
+        );
     }
 }
